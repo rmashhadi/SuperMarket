a3ff205b4dd21d48174ecdf7687bfeac44ba0e78
rmashhadi <rme1383@gmail.com>
2020-02-29 09:03:02 +0330

    Updates

---
+++ precache-manifest.4f275855d718b79cb6308dbc8e1f5c4a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "340a020e712d71cb8680514d9a616259",
+    "url": "/supermarket/index.html"
+  },
+  {
+    "revision": "e7eb56b55b06f2c31b6a",
+    "url": "/supermarket/static/css/2.a1d1da1a.chunk.css"
+  },
+  {
+    "revision": "7e81ba97b69d30c5ab97",
+    "url": "/supermarket/static/css/main.5ad11685.chunk.css"
+  },
+  {
+    "revision": "e7eb56b55b06f2c31b6a",
+    "url": "/supermarket/static/js/2.ff282689.chunk.js"
+  },
+  {
+    "revision": "e6edf5f5109ad7460f75a9f5716d868f",
+    "url": "/supermarket/static/js/2.ff282689.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e81ba97b69d30c5ab97",
+    "url": "/supermarket/static/js/main.1413c911.chunk.js"
+  },
+  {
+    "revision": "26d97ff9f573932211d4",
+    "url": "/supermarket/static/js/runtime-main.ce4125a7.js"
+  },
+  {
+    "revision": "509f14290a92bd618498bc8c5da1746d",
+    "url": "/supermarket/static/media/BRoya.509f1429.woff"
+  },
+  {
+    "revision": "e59513c82578df5dafb6b8da951be9b6",
+    "url": "/supermarket/static/media/Iranian Sans.e59513c8.ttf"
+  },
+  {
+    "revision": "36bc4f3bd36f0d51b0f532afb05610d8",
+    "url": "/supermarket/static/media/certificate.36bc4f3b.jpg"
+  },
+  {
+    "revision": "63a98dfdc21831949645a1bdda4e542a",
+    "url": "/supermarket/static/media/discount.63a98dfd.jpg"
+  },
+  {
+    "revision": "28dea628bc5d16e3c25273d78da1cbc4",
+    "url": "/supermarket/static/media/inst.28dea628.png"
+  },
+  {
+    "revision": "54b183253554ed043688694c110e4def",
+    "url": "/supermarket/static/media/ortho-wheelchair.54b18325.jpg"
+  },
+  {
+    "revision": "c397189487ad6b51b97e720ff8171a41",
+    "url": "/supermarket/static/media/printer.c3971894.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/supermarket/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/supermarket/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/supermarket/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/supermarket/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "009d78df00dd593abf3d606660bfab45",
+    "url": "/supermarket/static/media/special.009d78df.jpg"
+  },
+  {
+    "revision": "158c789a1288c712b2381f3a90900275",
+    "url": "/supermarket/static/media/street-wheelchair.158c789a.jpg"
+  },
+  {
+    "revision": "d4939a06dca773baf34916ca86476374",
+    "url": "/supermarket/static/media/wheelchair.d4939a06.jpg"
+  }
+]);
